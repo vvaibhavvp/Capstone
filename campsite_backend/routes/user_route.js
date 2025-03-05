a38f951d451862d24922beb69ca37d8129b37121
Darshika Patel <72062997+Darshika-1108@users.noreply.github.com>
2025-03-05 14:33:59 -0500

    Delete campsite_backend directory

--- campsite_backend/routes/user_route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { SignUp, login} from "../controller/user_controller.js";
-const router = express.Router();
-
-router.post("/signup", SignUp);
-router.post("/login", login);
-
-export default router;
